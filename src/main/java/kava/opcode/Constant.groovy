4a03254fba46de652b310562d037eeddf716dd51
kasonyang <i@kasonyang.com>
2015-08-16 11:01:08 +0800

    code clean

--- src/main/java/kava/opcode/Constant.groovy
+++ src/main/java/kava/opcode/Constant.groovy
@@ -1,12 +1,11 @@
 package kava.opcode;
 
 public class Constant {
-	//public static final int INT = 0,LONG=1,FLOAT = 2,DOUBLE = 3,CHAR=4,STRING=5,NULL=6;
-	//int type;
+	Integer id;
 	Object value;
 	String className;
 	public Constant(){
-		//this.type = type;
+		
 	}
 	public String toString(){
 		return value.toString();
