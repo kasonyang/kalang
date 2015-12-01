e3f9bcd7623f71ffa189e130e69a72dab694c655
kasonyang <i@kasonyang.com>
2015-12-01 22:53:11 +0800

    interface and type checked

--- src/main/groovy/compilier/TypeChecker.groovy
+++ src/main/groovy/compilier/TypeChecker.groovy
@@ -76,6 +76,17 @@ class TypeChecker extends AstVisitor<String> {
             fail("can not cast type:${from} => ${to}",node)
         }
     }
+    
+    public ClassNode loadAst(String name,AstNode node){
+        ClassNode ast = this.astLoader.getAst(name);
+        if(ast==null) fail("Unable to load class:",node)
+        return ast;
+    }
+    
+    String methodToString(MethodNode node){
+        String ptypes = getParameterTypes(node).join(",")
+        "${node.name}(${ptypes})"
+    }
 
     public void check(ClassNode clz){
         this.fieldTypes = [:]
@@ -84,6 +95,17 @@ class TypeChecker extends AstVisitor<String> {
         }
         this.clazz = clz
         visit(clazz)
+        if(clazz.interfaces?.size()>0){
+            for(def itfName in clazz.interfaces){
+                def itfNode = this.loadAst(itfName,clazz)
+                def unImps = getUnimplementedMethod(clazz,itfNode)
+                if(unImps?.size()>0){
+                    String mStr = methodToString(unImps.get(0));
+                    fail("unimplemented method:${mStr}",clazz);
+                }
+            }
+        }
+        //if(implemented(clazz))
     }
 
     @Override
@@ -158,7 +180,7 @@ class TypeChecker extends AstVisitor<String> {
         return expr;
     }
     
-    private boolean castable(String from,String to){
+    private  boolean castable(String from,String to){
         to = getClassType(to)
         from = getClassType(from)
         if(from==to) return true
@@ -191,8 +213,28 @@ class TypeChecker extends AstVisitor<String> {
         }
         return true
     }
+    
+    List<String> getParameterTypes(MethodNode mn){
+        List<String> types = []
+        for(ParameterNode p in mn.parameters){
+            types.add(p.type);
+        }
+        return types
+    }
+    
+    List<MethodNode> getUnimplementedMethod(ClassNode theClass,ClassNode theInterface){
+        List<MethodNode> list = []
+        for(MethodNode m in theInterface.methods){
+            String name = m.name;
+            List<String> types = getParameterTypes(m);
+            if(!getMethod(theClass,name,types)){
+                list.add(m);
+            }
+        }
+        return list;
+    }
 
-    private MethodNode getMethod(ClassNode ast,String name,List types){
+    private MethodNode getMethod(ClassNode ast,String name,List<String> types){
         def methods = ast.methods
         for(def m in methods){
             if(m.name!=name) continue
@@ -284,7 +326,7 @@ class TypeChecker extends AstVisitor<String> {
 
     @Override
     public String visitInvocationExpr(InvocationExpr node) {
-        List types = visit(node.arguments);
+        List<String> types = visit(node.arguments);
         String target = node.target?visit(node.target):this.clazz.name;
         String methodName = node.methodName;
         ClassNode ast = astLoader.getAst(target);
