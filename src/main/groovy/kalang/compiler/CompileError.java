dcdb8e207e0f0f9a60bcd7cc5cd49ad21b8ef1d4
kasonyang <i@kasonyang.com>
2015-12-14 14:00:41 +0800

    kl->j

--- src/main/groovy/kalang/compiler/CompileError.java
+++ src/main/groovy/kalang/compiler/CompileError.java
@@ -1,39 +1,24 @@
 package kalang.compiler;
 
 public class CompileError  extends java.lang.RuntimeException  {
-  public int start=0;
-  public int stop=0;
-  public java.lang.String className;
-  public java.lang.String source;
-  public  CompileError(java.lang.String msg,java.lang.String className,java.lang.String src,int start,int stop)  
-  {
-    super(msg);
-    this.start=start;
-    this.stop=stop;
-    this.className=className;
-    this.source=src;
-  }
-  
-  public java.lang.String getErrorSource()  
-  {
-    if((this.stop<0)){
-      return null;
+    public int start=0;
+    public int stop=0;
+    public java.lang.String className;
+    public java.lang.String source;
+    public  CompileError(java.lang.String msg,java.lang.String className,java.lang.String src,int start,int stop){
+        super(msg);
+        this.start=start;
+        this.stop=stop;
+        this.className=className;
+        this.source=src;
+        
     }
-    else {
-      
-    };
-    if((this.start<0)){
-      return null;
+    public java.lang.String getErrorSource(){
+        if((this.stop<0))return null;
+        if((this.start<0))return null;
+        if((this.stop<this.start))return null;
+        return this.source.substring(this.start,this.stop);
+        
     }
-    else {
-      
-    };
-    if((this.stop<this.start)){
-      return null;
-    }
-    else {
-      
-    };
-    return this.source.substring(this.start,this.stop);
-  }
+    
 }
