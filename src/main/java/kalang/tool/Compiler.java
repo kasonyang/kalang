6ecddb5030ca700388fadab362fcc722d213da60
KasonYang <im@kasonyang.com>
2016-06-29 21:08:28 +0800

    Fix return value of Compiler.compile

--- src/main/java/kalang/tool/Compiler.java
+++ src/main/java/kalang/tool/Compiler.java
@@ -114,7 +114,7 @@ public class Compiler {
             }
             hasError = false;
             fsc.compile();
-            return hasError;
+            return !hasError;
         }
     }
     
