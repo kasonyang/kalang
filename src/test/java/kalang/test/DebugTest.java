5eab404976ea94ac9b5ce49fbb94d4ae61bbe00c
KasonYang <im@kasonyang.com>
2016-08-12 13:13:04 +0800

    test ast2class

--- src/test/java/kalang/test/DebugTest.java
+++ src/test/java/kalang/test/DebugTest.java
@@ -2,6 +2,7 @@ package kalang.test;
 
 import java.io.File;
 import java.io.IOException;
+import kalang.compiler.codegen.Ast2Class;
 import org.junit.Test;
 
 /**
@@ -18,6 +19,7 @@ public class DebugTest extends JointCompilerTestCase {
         File debugDir = new File("debug");
         if(debugDir.exists()){
             this.addKalangAndJavaSourceDir(debugDir);
+            this.setCodeGenerator(new Ast2Class());
             compile();
         }
     }
