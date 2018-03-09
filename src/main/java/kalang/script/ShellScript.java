6bca184a894187eff38f60f6278ec3f3bd83f23a
kasonyang <me@kason.site>
2018-03-10 00:24:48 +0800

    refactor: add methods:readText,writeText

--- src/main/java/kalang/script/ShellScript.java
+++ src/main/java/kalang/script/ShellScript.java
@@ -1,20 +1,38 @@
 package kalang.script;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import kalang.lang.Script;
+import org.apache.commons.io.FileUtils;
 
 /**
  *
  * @author Kason Yang
  */
 public abstract class ShellScript extends Script {
-    
-    protected int exec(String command,String... arguments) throws IOException{
-        String[] list = new String[arguments.length+1];
+
+    public final static String DEFAULT_ENCODING = "utf8";
+
+    protected String readText(File file, String encoding) throws IOException {
+        return FileUtils.readFileToString(file, encoding);
+    }
+
+    protected String readText(File file) throws IOException {
+        return readText(file, DEFAULT_ENCODING);
+    }
+
+    protected void writeText(File file, String data, String encoding) throws IOException {
+        FileUtils.writeStringToFile(file, data, encoding);
+    }
+
+    protected void writeText(File file, String data) throws IOException {
+        writeText(file, data, DEFAULT_ENCODING);
+    }
+
+    protected int exec(String command, String... arguments) throws IOException {
+        String[] list = new String[arguments.length + 1];
         list[0] = command;
-        if(arguments.length>0){
+        if (arguments.length > 0) {
             System.arraycopy(arguments, 0, list, 1, arguments.length);
         }
         ProcessBuilder pb = new ProcessBuilder(list);
@@ -23,13 +41,13 @@ public abstract class ShellScript extends Script {
         try {
             p.waitFor();
         } catch (InterruptedException ex) {
-            
+
         }
         return p.exitValue();
     }
-    
-    protected int exec(String command) throws IOException{
-        return exec(command,new String[0]);
+
+    protected int exec(String command) throws IOException {
+        return exec(command, new String[0]);
     }
 
 }
