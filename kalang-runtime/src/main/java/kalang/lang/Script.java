e78472aa1fabbb362950dc63d4c29dccbf371ddd
kasonyang <me@kason.site>
2018-03-10 12:24:35 +0800

    feat: support arguments

--- kalang-runtime/src/main/java/kalang/lang/Script.java
+++ kalang-runtime/src/main/java/kalang/lang/Script.java
@@ -1,5 +1,7 @@
 package kalang.lang;
 
+import kalang.annotation.Nullable;
+
 /**
  *
  * @author Kason Yang
@@ -8,11 +10,24 @@ public abstract class Script {
 
     public final static int SUCCESS = 0;
 
+    private String[] arguments = new String[0];
+
     protected abstract void execute();
 
     public int run() {
         execute();
         return SUCCESS;
+    }
+
+    public int run(@Nullable String[] arguments) {
+        if (arguments != null) {
+            this.arguments = arguments;
+        }
+        return run();
+    }
+    
+    protected String[] arguments(){
+        return this.arguments;
     }
 
 }
