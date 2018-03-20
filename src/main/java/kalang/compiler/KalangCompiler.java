e5767e7d3e99905e64322efd69b7e5cbefb17706
kasonyang <me@kason.site>
2018-03-20 21:35:54 +0800

    refactor: refactor KalangCompiler class

--- src/main/java/kalang/compiler/KalangCompiler.java
+++ src/main/java/kalang/compiler/KalangCompiler.java
@@ -1,6 +1,5 @@
 package kalang.compiler;
 
-import java.io.PrintStream;
 import kalang.AstNotFoundException;
 import kalang.ast.ClassNode;
 import java.util.HashMap;
@@ -18,65 +17,72 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.DefaultErrorStrategy;
 import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RecognitionException;
+
 /**
  * The core compiler
- * 
+ *
  * @author Kason Yang
  */
-public class KalangCompiler extends AstLoader implements CompileContext{
-        
+public class KalangCompiler extends AstLoader implements CompileContext {
+
     private int compileTargetPhase = PHASE_ALL;
 
     private HashMap<String, CompilationUnit> compilationUnits = new HashMap<>();
 
     @Nonnull
     private final HashMap<String, KalangSource> sources = new HashMap();
-    
+
     private int compilingPhase;
-    
-    protected DiagnosisHandler diagnosisHandler = StandardDiagnosisHandler.INSTANCE;
-    
-    protected AstLoader astLoader;
-    
-    protected Configuration configuration = new Configuration();
+
+    private DiagnosisHandler diagnosisHandler = StandardDiagnosisHandler.INSTANCE;
+
+    private Configuration configuration = new Configuration();
+
+    private SourceLoader sourceLoader = new SourceLoader() {
+        @Override
+        public KalangSource loadSource(String className) {
+            return null;
+        }
+    };
 
     public KalangCompiler() {
         this(new JavaAstLoader());
     }
 
     public KalangCompiler(AstLoader astLoader) {
-        this.astLoader = astLoader;
+        super(astLoader);
     }
 
     /**
      * add source,which will be compiled
-     * 
+     *
      * @param className the name of class
      * @param source the source of class
      * @param fileName the file name of source
      */
-    public void addSource(String className, String source,String fileName) {
-        KalangSource src = new KalangSource(className, source,fileName);
+    public void addSource(String className, String source, String fileName) {
+        KalangSource src = new KalangSource(className, source, fileName);
         addSource(src);
     }
-    public void addSource(KalangSource source){
+
+    public void addSource(KalangSource source) {
         String className = source.getClassName();
         sources.put(className, source);
         compilationUnits.put(className, createCompilationUnit(source));
     }
-    
-    public void setCompileTargetPhase(int targetPhase){
+
+    public void setCompileTargetPhase(int targetPhase) {
         compileTargetPhase = targetPhase;
     }
 
     public int getCompileTargetPhase() {
         return compileTargetPhase;
     }
-    
-    public void compile(int targetPhase){
-        while(compilingPhase<targetPhase && compilingPhase < this.compileTargetPhase){
+
+    public void compile(int targetPhase) {
+        while (compilingPhase < targetPhase && compilingPhase < this.compileTargetPhase) {
             compilingPhase++;
-            for(CompilationUnit unit:compilationUnits.values()){
+            for (CompilationUnit unit : compilationUnits.values()) {
                 unit.compile(compilingPhase);
             }
         }
@@ -88,38 +94,44 @@ public class KalangCompiler extends AstLoader implements CompileContext{
     public void compile() {
         compile(compileTargetPhase);
     }
-    
-    private ClassNode findInnerClass(ClassNode topClass,String className){
-        for(ClassNode c:topClass.classes){
-            if(className.equals(c.name)) return c;
-            ClassNode ic = findInnerClass(c,className);
-            if(ic!=null) return ic;
+
+    private ClassNode findInnerClass(ClassNode topClass, String className) {
+        for (ClassNode c : topClass.classes) {
+            if (className.equals(c.name)) {
+                return c;
+            }
+            ClassNode ic = findInnerClass(c, className);
+            if (ic != null) {
+                return ic;
+            }
         }
         return null;
     }
 
     @Override
     protected ClassNode findAst(@Nonnull String className) throws AstNotFoundException {
-        String[] classNameInfo = className.split("\\$",2);
+        String[] classNameInfo = className.split("\\$", 2);
         String topClassName = classNameInfo[0];
         if (compilationUnits.containsKey(topClassName)) {
             CompilationUnit compilationUnit = compilationUnits.get(topClassName);
             ClassNode clazz = compilationUnit.getAst();
-            if(classNameInfo.length==1){
+            if (classNameInfo.length == 1) {
                 return clazz;
-            }else{
+            } else {
                 ClassNode c = findInnerClass(clazz, className);
-                if(c!=null) return c;
+                if (c != null) {
+                    return c;
+                }
             }
         }
-        SourceLoader sourceLoader = getSourceLoader();
-        if (sourceLoader != null) {
-            KalangSource source = sourceLoader.loadSource(className);
+        SourceLoader srcLoader = getSourceLoader();
+        if (srcLoader != null) {
+            KalangSource source = srcLoader.loadSource(className);
             if (source != null) {
                 return createCompilationUnit(source).getAst();
             }
         }
-        return astLoader.findAst(className);
+        return super.findAst(className);
     }
 
     @Nonnull
@@ -131,14 +143,14 @@ public class KalangCompiler extends AstLoader implements CompileContext{
     public HashMap<String, CompilationUnit> getCompilationUnits() {
         return compilationUnits;
     }
-    
+
     @Nullable
-    public CompilationUnit getCompilationUnit(@Nonnull String className){
+    public CompilationUnit getCompilationUnit(@Nonnull String className) {
         return compilationUnits.get(className);
     }
-    
-    protected CompilationUnit newCompilationUnit(KalangSource source){
-        return new CompilationUnit(source,this);
+
+    protected CompilationUnit newCompilationUnit(KalangSource source) {
+        return new CompilationUnit(source, this);
     }
 
     private CompilationUnit createCompilationUnit(KalangSource source) {
@@ -165,20 +177,20 @@ public class KalangCompiler extends AstLoader implements CompileContext{
     @Override
     public KalangParser createParser(CompilationUnit compilationUnit, CommonTokenStream tokenStream) {
         KalangParser parser = new KalangParser(tokenStream);
-        parser.setErrorHandler(new DefaultErrorStrategy(){
+        parser.setErrorHandler(new DefaultErrorStrategy() {
             @Override
             public void reportError(Parser recognizer, RecognitionException e) {
                 String msg = AntlrErrorString.exceptionString(recognizer, e);
                 Diagnosis diagnosis = new Diagnosis(
-                        compilationUnit.getCompileContext()
-                        , Diagnosis.Kind.ERROR
-                        , OffsetRangeHelper.getOffsetRange(e.getOffendingToken())
-                        , msg
-                        , compilationUnit.getSource()
+                        compilationUnit.getCompileContext(),
+                         Diagnosis.Kind.ERROR,
+                         OffsetRangeHelper.getOffsetRange(e.getOffendingToken()),
+                         msg,
+                         compilationUnit.getSource()
                 );
                 diagnosisHandler.handleDiagnosis(diagnosis);
             }
-            
+
         });
         return parser;
     }
@@ -199,18 +211,17 @@ public class KalangCompiler extends AstLoader implements CompileContext{
     }
 
     @Override
-    public AstLoader getAstLoader() {
+    public final AstLoader getAstLoader() {
         return this;
     }
 
     @Override
     public SourceLoader getSourceLoader() {
-        return new SourceLoader() {
-            @Override
-            public KalangSource loadSource(String className) {
-                return null;
-            }
-        };
+        return this.sourceLoader;
+    }
+
+    public void setSourceLoader(SourceLoader sourceLoader) {
+        this.sourceLoader = sourceLoader;
     }
 
     @Override
