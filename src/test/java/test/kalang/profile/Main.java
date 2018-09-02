a69a3e9ad938b50b46d6741d7b857c5c1b07f0b0
kasonyang <me@kason.site>
2018-09-02 12:29:04 +0800

    chore: add Main class for profile

---
+++ src/test/java/test/kalang/profile/Main.java
@@ -0,0 +1,17 @@
+package test.kalang.profile;
+
+import java.io.IOException;
+import kalang.test.DebugTest;
+
+/**
+ *
+ * @author Kason Yang
+ */
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        DebugTest dt = new DebugTest();
+        dt.test();
+    }
+
+}
