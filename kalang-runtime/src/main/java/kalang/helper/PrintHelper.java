240e4db6d9b8035c498873065710cc2530ad7517
lbqh <>
2018-12-15 16:41:18 +0800

    refactor: add printlns

--- kalang-runtime/src/main/java/kalang/helper/PrintHelper.java
+++ kalang-runtime/src/main/java/kalang/helper/PrintHelper.java
@@ -1,9 +1,17 @@
 package kalang.helper;
 
+import java.util.Collection;
+
 public class PrintHelper {
 
     public static void println(Object msg) {
         System.out.println(msg);
+    }
+
+    public static void printlns(Collection messages) {
+        for (Object o : messages) {
+            println(o);
+        }
     }
 
     public static void printlns(Object[] messages) {
