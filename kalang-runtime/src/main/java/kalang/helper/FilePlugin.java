81cfba40f6884fc6f43ad2dea5478102741f54d9
kasonyang <me@kason.site>
2019-07-30 23:18:07 +0800

    feat: refactor FilePlugin

--- kalang-runtime/src/main/java/kalang/helper/FilePlugin.java
+++ kalang-runtime/src/main/java/kalang/helper/FilePlugin.java
@@ -1,16 +1,14 @@
 package kalang.helper;
 
-import kalang.annotation.Nullable;
 import kalang.annotation.PluginMethod;
 
 import java.io.*;
+import java.nio.charset.Charset;
 
 public class FilePlugin {
 
-    public static String DEFAULT_ENCODING = "utf-8";
-
     @PluginMethod
-    public static String readToString(File file, @Nullable String charset) throws IOException {
+    public static byte[] readToBytes(File file) throws IOException {
         try(InputStream isr = new FileInputStream(file)) {
             long fileLen = file.length();
             if (fileLen > Integer.MAX_VALUE) {
@@ -19,30 +17,32 @@ public class FilePlugin {
             int len = (int) fileLen;
             byte[] data = new byte[len];
             isr.read(data);
-            return new String(data, charset);
+            return data;
         }
+    }
+
+    @PluginMethod
+    public static String readToString(File file,String charset) throws IOException {
+        return new String(readToBytes(file),charset);
     }
 
     @PluginMethod
     public static String readToString(File file) throws IOException {
-        return readToString(file, DEFAULT_ENCODING);
+        return readToString(file,Charset.defaultCharset().name());
     }
 
     @PluginMethod
-    public static void write(File file, String data, @Nullable String charset,boolean append) throws IOException {
-        try(FileOutputStream fos = new FileOutputStream(file,append)){
-            fos.write(data.getBytes(charset));
+    public static void write(File file,byte[] data) throws IOException {
+        try(FileOutputStream fos = new FileOutputStream(file)){
+            fos.write(data);
         }
     }
 
     @PluginMethod
-    public static void write(File file, String data, @Nullable String charset) throws IOException {
-        write(file,data,charset,false);
-    }
-
-    @PluginMethod
-    public static void write(File file, String data) throws IOException {
-        write(file, data, DEFAULT_ENCODING);
+    public static void append(File file,byte[] data) throws IOException {
+        try(FileOutputStream fos = new FileOutputStream(file,true)){
+            fos.write(data);
+        }
     }
 
 }
