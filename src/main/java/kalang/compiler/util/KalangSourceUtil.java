7bae60a49866fc0895790cc94b73039d04b4f0a2
kasonyang <me@kason.site>
2019-10-01 22:31:58 +0800

    fix: encoding

--- src/main/java/kalang/compiler/util/KalangSourceUtil.java
+++ src/main/java/kalang/compiler/util/KalangSourceUtil.java
@@ -11,9 +11,9 @@ import java.io.IOException;
  * @author Kason Yang 
  */
 public class KalangSourceUtil {
-    public static KalangSource create(File dir,File sourceFile) throws IOException{
+    public static KalangSource create(File dir,File sourceFile, String encoding) throws IOException{
         String clsName = ClassNameUtil.getClassName(dir, sourceFile);
-        KalangSource ks = new KalangSource(clsName,FileUtils.readFileToString(sourceFile),sourceFile.getName());
+        KalangSource ks = new KalangSource(clsName,FileUtils.readFileToString(sourceFile, encoding),sourceFile.getName());
         return ks;        
     }
 }
